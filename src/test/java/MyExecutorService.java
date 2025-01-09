ada179b178b810c591ac9d3f70e2377276fe6163
xianchaoye <18201082822@163.com>
2025-01-09 17:08:49 +0800

    自定义实现一个ExecutorService

---
+++ src/test/java/MyExecutorService.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MyExecutorService {
+
+    private final BlockingQueue queue;
+
+    private Thread thread;
+
+    public MyExecutorService() {
+        queue = new LinkedBlockingQueue();
+        thread = new Thread(() -> {
+            while (true) {
+                try {
+                    Runnable runnable = (Runnable) queue.take();
+                    runnable.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "myExecutorService");
+        thread.start();
+    }
+
+    public void submit(Runnable runnable) {
+        this.queue.offer(runnable);
+    }
+
+
+    public static void main(String[] args) {
+        MyExecutorService executorService = new MyExecutorService();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                int finalI = i;
+                executorService.submit(() -> System.out.println(Thread.currentThread().getName() + ":" + finalI));
+            }
+        }, "thread1");
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 10; i < 20; i++) {
+                int finalI = i;
+                executorService.submit(() -> System.out.println(Thread.currentThread().getName() + ":" + finalI));
+            }
+        }, "thread2");
+        thread1.start();
+        thread2.start();
+    }
+}
